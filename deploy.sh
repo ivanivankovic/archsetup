54664eb202dacfcd14765fe047859fe1e76535ce
ivanivankovic <ivankovicivan11@gmail.com>
2021-08-14 22:41:23 +0200

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 sudo pacman -Syu --noconfirm bspwm virtualbox-guest-utils git rxvt-unicode terminator arandr rofi pulseaudio pavucontrol lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xdotool flameshot firefox sxhkd man-db htop wget curl zsh powerline-fonts
 cd /opt ; sudo git clone https://aur.archlinux.org/yay.git ; sudo chown -R arch:arch yay ; cd yay ; makepkg -si --noconfirm
 yay -aS --noconfirm --answerdiff=None polybar
-cd /opt ; sudo git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ; sudo chown arch:arch -R polybar-themes ; cd /opt/polybar-themes ; echo 2 | ./setup.sh ; mv $HOME/.config/polybar.old $HOME/.config/polybar
+cd /opt ; sudo git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ; sudo chown arch:arch -R polybar-themes ; cd /opt/polybar-themes ; echo 2 | ./setup.sh ; rm -r $HOME/.config/polybar ; mv $HOME/.config/polybar.old $HOME/.config/polybar
 sudo systemctl enable lightdm
 yay -aS --noconfirm --answerdiff=None betterlockscreen
 yay -aS --noconfirm --answerdiff=None alacritty-git
 yay -aS --noconfirm --answerdiff=None alacritty-themes
 alacritty-themes --create
-alacritty-themes Atelierlakeside.dark
+alacritty-themes Cobalt-2
 sudo pacman -S alsa-utils speaker-test -c2 alsamixer
 sudo rm -r /etc/X11
 sudo ln -s $HOME/.config/X11 /etc/X11
@@ -29,3 +29,7 @@ ln -s $HOME/.config/zsh/zshrc $HOME/.zshrc
 yay -aS --noconfirm --answerdiff=None antigen-git
 curl -L git.io/antigen > antigen.zsh
 sudo pacman -S fzf
+sudo pacman -S tmux
+sudo pacman -S compton
+sudo pacman -S powerline powerline-fonts
+sudo pacman -S noto-fonts-emoji
